23ff423e6533181f240717951a33d768439b6ee8
NAVEED476 <snaveedn81@gmail.com>
2024-10-14 12:45:56 +0530

    balanced paranthesisi

--- Solutions/BalancingParanthesis.js
+++ Solutions/BalancingParanthesis.js
@@ -1,30 +1,54 @@
-function isBalanced(str) {
-  let stack = [];
+// function isBalanced(str) {
+//   let stack = [];
 
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] == "(" || str[i] === "{" || str[i] === "[") {
-      stack.push(str[i]);
-    } else {
-      if (stack.length === 0) {
-        return false;
-      } else {
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] == "(" || str[i] === "{" || str[i] === "[") {
+//       stack.push(str[i]);
+//     } else {
+//       if (stack.length === 0) {
+//         return false;
+//       } else {
+//         const top = stack.pop();
+//         if (
+//           (str[i] === ")" && top !== "(") ||
+//           (str[i] === "]" && top !== "[") ||
+//           (str[i] === "}" && top !== "{")
+//         ) {
+//           return false;
+//         }
+//       }
+//     }
+//   }
+
+//   return stack.length === 0;
+// }
+
+const str1 = "[()]{}";
+const str2 = "[(])";
+
+// console.log(isBalanced(str1));
+// console.log(isBalanced(str2));
+
+
+function isBalanced(str){
+  let stack = [];
+  for(let i=0;i<str.lenght;i++){
+    if(str[i] ==="(" || str[i] ==="{", str[i]==="]"){
+      stack.push(str[i])
+    }else{
+      if(stack.lenght ===0)return false
+      else{
         const top = stack.pop();
-        if (
-          (str[i] === ")" && top !== "(") ||
-          (str[i] === "]" && top !== "[") ||
-          (str[i] === "}" && top !== "{")
-        ) {
-          return false;
+        if((str[i] === ")" && top==="(") ||
+        (str[i] ==="}" && top ==="{") || str[i] === "]" && top ==="]"){
+          return false
         }
       }
     }
   }
-
-  return stack.length === 0;
+  return stack.length ===0;
 }
 
-const str1 = "[()]{}";
-const str2 = "[(])";
 
 console.log(isBalanced(str1));
 console.log(isBalanced(str2));
