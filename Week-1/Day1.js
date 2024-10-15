05ca7f427e950e152c65ae87ae4fba637525daf3
NaveedYW <naveed.shaik@yellowowl.app>
2024-10-15 22:50:53 +0530

    some basic pratice

--- Week-1/Day1.js
+++ Week-1/Day1.js
@@ -1,2 +1,21 @@
-// objects 
+// how to check the variable is an array
 
+let arr = [];
+
+console.log(Array.isArray(arr));
+
+
+// how to combine two arrays
+
+let arr1 = [1,2,3]
+let arr2 = [4,5,6]
+
+console.log([...arr1,...arr2]);
+
+// find the index of the praticluar element
+
+const arr3 = ['apple',"banana","mango"];
+
+// find banana index
+
+console.log('banana index',arr3.indexOf('banana'))
