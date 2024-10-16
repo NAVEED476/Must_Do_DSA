85883d9a24d50745408593dc86f1b2af2e3bd56d
NaveedYW <naveed.shaik@yellowowl.app>
2024-10-16 23:01:18 +0530

    includes methid returns true or false

--- Week-1/Day1.js
+++ Week-1/Day1.js
@@ -12,10 +12,14 @@ let arr2 = [4,5,6]
 
 console.log([...arr1,...arr2]);
 
-// find the index of the praticluar element
+// find the index of the praticluar element!
 
 const arr3 = ['apple',"banana","mango"];
 
 // find banana index
 
 console.log('banana index',arr3.indexOf('banana'))
+
+const arr4 = [1,2,3,4]
+
+console.log(arr4.includes(3));
